8b6513c99486cfb14b4679fe728b4bde2e045794
IanChiu-333 <ianchiu333@gmail.com>
2024-06-26 19:59:23 -0700

    Fixed and finalized 3/4 checks and fixes

--- src/planetsca/simplify_aoi.py
+++ src/planetsca/simplify_aoi.py
@@ -6,16 +6,15 @@ from shapely import concave_hull, unary_union
 from shapely.geometry import Polygon, mapping
 
 
-
 def get_coordinates(file_path: str) -> list:
     """
-    Helper method for extracting a list of coordinates from a GeoJSON file
+    Gets coordinates from a GeoJSON file
 
     Parameters:
-    - file_path: The path to the GeoJSON file.
+    - file_path: The path to the GeoJSON file
 
     Returns:
-    - list: Coordinates of a given GeoJSON file
+    - list: List of vertices from GeoJSON polygon
     """
     with open(file_path) as f:
         data = json.load(f)
@@ -38,7 +37,6 @@ def get_coordinates(file_path: str) -> list:
                     coordinates_list.extend(polygon)
     return coordinates_list
 
-
 def vertex_count(file_path: str) -> int:
     """
     Counts vertexes from a GeoJSON file.
@@ -53,6 +51,13 @@ def vertex_count(file_path: str) -> int:
 
 
 def reduce_vertex(file_path: str, ratio: int):
+    """
+    Reduces the number of vertices in a polygon
+
+    Parameters:
+    - file_path: The path to the GeoJSON file
+    - ratio: Sets the concave_hull ratio
+    """
     with fiona.open(file_path) as collection:
        hulls = [concave_hull(shape(feat["geometry"]), ratio) for feat in collection]
         
@@ -62,21 +67,25 @@ def reduce_vertex(file_path: str, ratio: int):
         json.dump(dissolved_hulls, f)
 
 
-def check_holes(file_path: str) -> bool:
+def check_hole(file_path: str) -> bool:
     """
-    Checks if GeoJSON has holes by comparing the first and last entry of coordinates.
+    Checks if a polygon contains a hole.
 
     Parameters:
     - file_path: The path to the GeoJSON file.
 
     Returns:
-    - bool: True if there are holes, false if there are no holes
+    - bool: True if there is a hole in the polygon, false if there is not
     """
-    coordinates_list = get_coordinates(file_path)
-    print(coordinates_list)
-    return coordinates_list[0] != coordinates_list[-1]
+    with open(file_path) as f:
+        geojson = json.load(f)
 
-check_holes("overlapping_test.geojson")
+    for feature in geojson['features']:
+        if feature['geometry']['type'] == 'Polygon':
+            coordinates = feature['geometry']['coordinates']
+            if len(coordinates) > 1:
+                return True
+    return False
 
 
 def fill_holes(file_path: str):
@@ -117,13 +126,13 @@ def fill_holes(file_path: str):
 
 def check_overlap(file_path: str) -> bool:
     """
-    Checks if two polygons overlap 
+    Checks if two polygons are overlapping from a GeoJSON file
 
     Parameters:
     - file_path: The path to the GeoJSON file.
 
-    Returns:
-    - bool: True if there is an overlap, false if there is no overlap
+     Returns:
+    - bool: True if there is overlap and false if there is no overlap
     """
     coordinates_list = get_coordinates(file_path)
     polygon1 = []
@@ -144,7 +153,44 @@ def check_overlap(file_path: str) -> bool:
             break
     
     return Polygon(polygon1).intersects(Polygon(polygon2))
-    
 
 
+def fix_overlap(file_path: str):
+    """
+    Fixes the overlap of two polygons by deleting the overlapped area and merging the two polygons into one polygon
+
+    Parameters:
+    - file_path: The path to the GeoJSON file.
+    """
+    coordinates_list = get_coordinates(file_path)
+    polygon1 = []
+    polygon2 = []
+    first_entry = coordinates_list[0]
+    polygon1.append(first_entry)
+    divider = 0
+    for coordinate in coordinates_list[1:]:
+        polygon1.append(coordinate)
+        if (coordinate == first_entry):
+            divider = coordinates_list[1:].index(coordinate) + 1
+            break
+    first_entry = coordinates_list[divider]
+    for coordinate in coordinates_list[divider+1:]:
+        polygon2.append(coordinate)
+        if (coordinate == first_entry):
+            divider = coordinates_list[1:].index(coordinate) + 1
+            break
+    combined_polygon = unary_union([Polygon(polygon1), Polygon(polygon2)])
+
+    geojson = {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "type": "Feature",
+                "geometry": mapping(combined_polygon),
+                "properties": {}
+            }
+        ]
+    }
     
+    with open('corrected_overlap.geojson', 'w') as f:
+        json.dump(geojson, f)
